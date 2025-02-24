481d079644cf6d83fec3e323564b27af43bc0054
ahmedalkholy49 <ahmedalkholy49@gmail.com>
2025-02-24 18:49:39 +0200

    adding express app

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+
+import * as express from "express";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen( PORT, () => console.log("API Gateway Started at: " + PORT) );
