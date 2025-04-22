8d41c27aa8c1d17f811368c4b7296a2b709ff4bf
ahmedalkholy49 <ahmedalkholy49@gmail.com>
2025-04-23 00:22:01 +0200

    feat: use cors to only allow the origin specified in .env

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 
 import express from "express";
 import dotenv from "dotenv";
+import cors from 'cors';
 dotenv.config();
 
 import router from './routes/api';
 
 const app = express();
+
+app.use(cors( {
+    origin: process.env.CORS_ORIGIN || '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: process.env.CORS_CREDENTIALS === 'true'
+}));
 
 app.use(express.json());
 app.use('/api', router);
